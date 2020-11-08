c9f641e1f9fa4827318528c0c0ffcffa0977e5ed
Jared Suresky <jsuresky@gmail.com>
2020-11-08 14:00:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,57 @@
 //Bring in the Postgress library
 const pg = require("pg");
 
+//Here is where express fits in...
+const express = require("express");
+const app = express();
+
+//configuring the express pipeline
+app.get("/", async (req, res, next) => {
+  try {
+    const response = await client.query('SELECT * FROM "Sport";'); //the response that we're getting from our Postgress Server.
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+const port = process.env.PORT || 3000; //set up a port...or if there is no port for us, choose port 3000
+
 //Now, we want to create a postgress client
 const client = new pg.Client("postgress://localhost/cards_db");
 
+/*
+below, "Sport" is the parent table and
+"Athlete is the child table" We basically say that
+an athlete belong to a sport. 
+*/
 const syncAndSeed = async () => {
   const SQL = `
-  DROP TABLE IF EXISTS "Sport";  
+  DROP TABLE IF EXISTS "Athlete";
+  DROP TABLE IF EXISTS "Sport";
   CREATE TABLE "Sport" (
         id INTEGER PRIMARY KEY,
         name VARCHAR(100) NOT NULL
     );
-    CREATE TABLE "Sport" (
+    CREATE TABLE "Athlete" (
         id INTEGER PRIMARY KEY,
-        name VARCHAR(100) NOT NULL
+        name VARCHAR(100) NOT NULL,
+        sport_id INTEGER REFERENCES "Sport"(id)
     );
     INSERT INTO "Sport"(id,name) VALUES(1, 'Baseball');
     INSERT INTO "Sport"(id,name) VALUES(2, 'Football');
     INSERT INTO "Sport"(id,name) VALUES(3, 'Basketball');
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(1, 'David Wright', 1);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(2, 'Derek Jeter', 1);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(3, 'Dr. Fauci', 1);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(4, 'Pedro Martinez', 1);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(5, 'Randy Johnson', 1);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(6, 'Barry Sanders', 2);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(7, 'Peyton Manning', 2);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(8, 'Michael Irving', 2);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(9, 'Jason Kidd', 3);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(10, 'Scotty Pippen', 3);
+    INSERT INTO "Athlete"(id,name, sport_id) VALUES(11, 'Karl Malone', 3);
     `;
   await client.query(SQL);
 };
@@ -27,6 +61,8 @@ const setUp = async () => {
   try {
     await client.connect();
     await syncAndSeed();
+    console.log("You are connected to the database.");
+    app.listen(port, () => console.log(`Listening on port ${port}`)); //if successful, console log a text string
   } catch (ex) {
     console.log(ex);
   }
